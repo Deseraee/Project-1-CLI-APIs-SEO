d3956a2a46e77f78207b7ec418ceabc7a90b917e
Deserae Mendoza <deseraemendoza857@gmail.com>
2026-06-24 03:24:12 +0000

    new updates

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import requests
 
 connection = sqlite3.connect("mydatabase.db")
 curr = connection.cursor()
@@ -7,14 +8,33 @@ username = input("Enter your username: ")
 name = input("Enter your name: ")
 location = input("Enter your location: ")
 
-#if the username is already in the databse it ask the user if their location
-# is the current one assoicated with the username 
-# yes - same user
-#no new user and tell me that username is already taken
 curr.execute(
   "INSERT INTO usersInfo (username, name, location) VALUES (?, ?, ?)",
   (username,name,location)
 )
+
+
+#if the username is already in the databse it ask the user if their location
+# is the current one assoicated with the username 
+# yes - same user
+#no new user and tell me that username is already taken
+#if username in databse:
+ # reply = input("Is your location {location} ? yes or no: ")
+  #curr.execute("INSERT INTO usersInfo")
+
+OPENWEATHER_API_KEY ="10f5834bd984c8ed9f420584d0771d67"
+url = url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHER_API_KEY}&units=metric"
+respond = requests.get(url)
+location_weather = respond.json()
+
+if respond.status_code == 200:
+  temperature = location_weather["main"]["temp"]
+  description = location_weather["weather"][0]["description"]
+  humidity = location_weather["main"]["humidity"] 
+  
+  print(f"The weather in your location is {temperature}°C, with {description}, and a humidity of {humidity}%")
+
+
 
 connection.commit()
 connection.close()
